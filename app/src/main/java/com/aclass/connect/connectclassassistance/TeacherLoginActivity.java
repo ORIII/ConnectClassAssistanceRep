ec4dd93c16f10dafcad214cae6acb3ee2d20d502
Danny Wilimsius <wilimsiusdanny@gmail.com>
2018-01-20 22:41:11 +0700

    first commit yoo

---
+++ app/src/main/java/com/aclass/connect/connectclassassistance/TeacherLoginActivity.java
@@ -0,0 +1,38 @@
+package com.aclass.connect.connectclassassistance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TeacherLoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button mTeacherLogin, mStudentLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_login);
+
+        mTeacherLogin = findViewById(R.id.teacherLogin);
+        mStudentLogin = findViewById(R.id.studentLogin);
+
+        mTeacherLogin.setOnClickListener(this);
+        mStudentLogin.setOnClickListener(this);
+
+    }
+
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.teacherLogin:
+                startActivity(new Intent(this, TeacherLoginActivity.class));
+                break;
+            case R.id.studentLogin:
+                startActivity(new Intent(this, StudentLoginActivity.class));
+                break;
+        }
+    }
+}
